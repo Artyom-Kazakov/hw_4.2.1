92ffde62b884db882ff9c7146422aa2574043205
Artyom-Kazakov <kasakkainen@mail.ru>
2022-06-03 23:55:35 +0300

    First commit

---
+++ hw_4.2.1/hw_4.2.1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+enum class months {
+	exit = 0,
+	январь,
+	февраль,
+	март,
+	апрель,
+	май,
+	июнь,
+	июль,
+	август,
+	сентябрь,
+	октябрь,
+	ноябрь,
+	декабрь
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int month_input;
+	std::cout << "Введите номер месяца: ";
+	std::cin >> month_input;
+
+	do {
+		if ((month_input >= static_cast<int>(months::январь)) && (month_input <= static_cast<int>(months::декабрь))) {
+			std::cout << "Введите номер месяца: ";
+			std::cin >> month_input;
+		}
+		if ((month_input > static_cast<int>(months::декабрь)) || (month_input < static_cast<int>(months::январь))) {
+			std::cout << "Неверно! Введите номер месяца: ";
+			std::cin >> month_input;
+		}
+	} while (month_input != static_cast<int>(months::exit));
+	
+	std::cout << "До свидания!";
+}
