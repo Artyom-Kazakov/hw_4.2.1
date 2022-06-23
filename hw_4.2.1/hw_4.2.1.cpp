7d18ec2abe4ce75d1db91c40317bd29a24cd15ee
Artyom-Kazakov <kasakkainen@mail.ru>
2022-06-23 18:41:57 +0300

    first commit

--- hw_4.2.1/hw_4.2.1.cpp
+++ hw_4.2.1/hw_4.2.1.cpp
@@ -1,19 +1,18 @@
 ﻿#include <iostream>
 
 enum class months {
-	exit = 0,
-	январь,
-	февраль,
-	март,
-	апрель,
-	май,
-	июнь,
-	июль,
-	август,
-	сентябрь,
-	октябрь,
-	ноябрь,
-	декабрь
+	jan = 1,
+	feb,
+	mar,
+	apr,
+	may,
+	jun,
+	jul,
+	aug,
+	sep,
+	oct,
+	nov,
+	dec
 };
 
 int main() {
@@ -23,15 +22,50 @@ int main() {
 	std::cin >> month_input;
 
 	do {
-		if ((month_input >= static_cast<int>(months::январь)) && (month_input <= static_cast<int>(months::декабрь))) {
+			if ((month_input == static_cast<int>(months::jan))) {
+				std::cout << "Январь" << std::endl;
+			};
+			if ((month_input == static_cast<int>(months::feb))) {
+				std::cout << "Февраль" << std::endl;
+			};
+			if ((month_input == static_cast<int>(months::mar))) {
+				std::cout << "Март" << std::endl;
+			};
+			if ((month_input == static_cast<int>(months::apr))) {
+				std::cout << "Апрель" << std::endl;
+			};
+			if ((month_input == static_cast<int>(months::may))) {
+				std::cout << "Май" << std::endl;
+			};
+			if ((month_input == static_cast<int>(months::jun))) {
+				std::cout << "Июнь" << std::endl;
+			};
+			if ((month_input == static_cast<int>(months::jul))) {
+				std::cout << "Июль" << std::endl;
+			};
+			if ((month_input == static_cast<int>(months::aug))) {
+				std::cout << "Август" << std::endl;
+			};
+			if ((month_input == static_cast<int>(months::sep))) {
+				std::cout << "Сентябрь" << std::endl;
+			};
+			if ((month_input == static_cast<int>(months::oct))) {
+				std::cout << "Октябрь" << std::endl;
+			};
+			if ((month_input == static_cast<int>(months::nov))) {
+				std::cout << "Ноябрь" << std::endl;
+			};
+			if ((month_input == static_cast<int>(months::dec))) {
+				std::cout << "Декабрь" << std::endl;
+			}
 			std::cout << "Введите номер месяца: ";
 			std::cin >> month_input;
-		}
-		if ((month_input > static_cast<int>(months::декабрь)) || (month_input < static_cast<int>(months::январь))) {
+		
+		if ((month_input > static_cast<int>(months::dec)) || (month_input < static_cast<int>(months::jan))) {
 			std::cout << "Неверно! Введите номер месяца: ";
 			std::cin >> month_input;
 		}
-	} while (month_input != static_cast<int>(months::exit));
+	} while (month_input != 0);
 	
 	std::cout << "До свидания!";
 }
